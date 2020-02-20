f19d74137f22c1f8ac914257407ec9fc3d567e33
Dagel <giovanna@velabikes.com.br>
2020-02-20 16:37:21 -0300

    draweritem navigation/route fix and route for device page

--- examples/react-native/App.js
+++ examples/react-native/App.js
@@ -5,19 +5,22 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import Dashboard from './screens/Dashboard'
 import Profile from './screens/Profile'
 import Styleguide from './screens/Styleguide'
+import Device from './screens/Device'
 import { Icons } from '@vela/ui'
 
 const Drawer = createDrawerNavigator()
 
-function DrawerContent ({state, descriptors}) {
+function DrawerContent ({navigation, state, descriptors}) {
   return state.routes.map(route => {
     const { drawerLabel, drawerIcon } = descriptors[route.key].options
-    return <DrawerItem icon={drawerIcon} label={drawerLabel} {...route} />
+    return <DrawerItem icon={drawerIcon} label={drawerLabel} routeName={route.name} navigation={navigation} />
   })
 }
 
-function DrawerItem ({label, icon}) {
-  return <Button label={label} icon={icon}  />
+const DrawerItem = ({ navigation, icon, label, routeName}) => {
+  return (
+    <Button onPress={() => navigation.navigate(routeName)} icon={icon} label={label} />
+  )
 }
 
 function App () {
@@ -27,13 +30,19 @@ function App () {
         <Drawer.Screen name="Dashboard" component={Dashboard} 
             options={{
               drawerLabel: 'Home',
-              drawerIcon: (Icons.Logo)
+              drawerIcon: (Icons.Location)
             }} 
         />      
         <Drawer.Screen name="Profile" component={Profile} 
           options={{
             drawerLabel: 'Meu Perfil',
             drawerIcon: (Icons.User)
+            }} 
+        />
+        <Drawer.Screen name="Device" component={Device} 
+          options={{
+            drawerLabel: 'Minha Vela',
+            drawerIcon: (Icons.Logo)
             }} 
         />
         <Drawer.Screen name="Styleguide" component={Styleguide} 
