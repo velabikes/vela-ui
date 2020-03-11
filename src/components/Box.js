bf51eeaed4f476518765a646a5f94952dcb7ba1e
Diogo V. Araújo <diogovaraujo@gmail.com>
2020-03-10 21:51:41 -0300

    Styling header

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {View} from 'react-native'
 import {useTheme} from '../lib/theme'
 
-const Box = ({children, style}) => {
+const Box = ({children, style, ...props}) => {
   const {colors, metrics} = useTheme()
   const baseStyle = {
     backgroundColor: colors.background,
     padding: metrics.padding
   }
-  return <View style={[baseStyle, style]}>{children}</View>
+  return <View style={[baseStyle, style]} {...props}>{children}</View>
 }
 
 export default Box
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import {SafeAreaView, View} from 'react-native'
 import Box from "../Box"
 import HeaderMenuButton from "./HeaderMenuButton"
 import Text from "../Text"
 
+const boxStyle = {
+  flexDirection: 'row',
+  justifyContent: 'space-evenly'
+}
+
 export default function Header() {
   return (
     <Box>
-      <Text>asdf</Text>
-      <HeaderMenuButton />
+      <SafeAreaView style={boxStyle}>
+        <HeaderMenuButton />
+        <Text>Title</Text>
+        <View />
+      </SafeAreaView>
     </Box>
   )
 }
