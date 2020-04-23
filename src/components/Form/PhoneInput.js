ca281d2633162fca26bd5db5c47701951846b084
Guilherme Oliveira <guilherme-lamon@hotmail.com>
2020-04-23 16:27:05 -0300

    Create mask phonenumber and fixing bugs

--- src/components/Form/PhoneInput.js
+++ src/components/Form/PhoneInput.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import TextInput from './TextInput'
 
-import StringMask from 'string-mask'
-
 //import { TextInputMask } from 'react-native-masked-text'
 
-const phoneFormat = phone => {
-  const newPhone = phone.replace(/([^\d])+/gim, '')
-  return newPhone
-}
+const phoneFormat = phone => phone.replace(/([^\d])+/gim, '')
+const maskPhone = value => value.replace(/^(\d{2})(\d{4,5})(\d{1,4}).*/,"$1 $2-$3")
 
-const maskPhone = value => {
-  const formatter = new StringMask('(00) 00000-0000')
-  const result = formatter.apply(value)
-  return result
-}
 
 const PhoneInput = ({onChangeText, value, ...props }) => {
-
-  console.log(props)
   return (
     <TextInput
       onChangeText={text => {
-        phoneFormat(text)
+        onChangeText(phoneFormat(text))
       }}
       value={maskPhone(value)}
       keyboardType='numeric'
