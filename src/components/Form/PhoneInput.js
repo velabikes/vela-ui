9eeff55429ffbe3308a66cd412a9e8b7297c5294
Guilherme Oliveira <guilherme-lamon@hotmail.com>
2020-04-23 14:10:16 -0300

    npm string-mask error

--- src/components/Form/PhoneInput.js
+++ src/components/Form/PhoneInput.js
@@ -1,5 +1,34 @@
-import TextInput from './TextInput'
 import React from 'react'
+import TextInput from './TextInput'
+
+import StringMask from 'string-mask'
+
 //import { TextInputMask } from 'react-native-masked-text'
 
-export default TextInput
+const phoneFormat = phone => {
+  const newPhone = phone.replace(/([^\d])+/gim, '')
+  return newPhone
+}
+
+const maskPhone = value => {
+  const formatter = new StringMask('(00) 00000-0000')
+  const result = formatter.apply(value)
+  return result
+}
+
+const PhoneInput = ({onChangeText, value, ...props }) => {
+
+  console.log(props)
+  return (
+    <TextInput
+      onChangeText={text => {
+        phoneFormat(text)
+      }}
+      value={maskPhone(value)}
+      keyboardType='numeric'
+      {...props}
+    />
+  )
+}
+
+export default PhoneInput
