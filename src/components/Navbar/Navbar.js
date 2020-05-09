bbae87f4931f03e5df20c421d9c8b2926a57f943
Diogo Vianna <diogo@velabikes.com.br>
2020-05-09 17:21:09 -0300

    Better safe area handling.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,34 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { useSafeArea } from "react-native-safe-area-context";
 import Box from "../Box";
 import HeaderMenuButton from "./NavbarMenuButton";
 import HeaderBackButton from "./NavbarBackButton";
 import Heading from "../Typo/Heading";
 
 export default function Navbar({ scene, previous }) {
-  const { options, navigation } = scene.descriptor
-  const title = options.title || scene.route.name
-  const { onBack } = options
+  const { options, navigation } = scene.descriptor;
+  const title = options.title || scene.route.name;
+  const { onBack } = options;
+  const insets = useSafeArea();
 
   return (
-    <Box background='foreground'>
-      <SafeAreaView>
-        <Box pad="large" direction="row" align="center">
-          <Box flex>
-            { navigation && navigation.openDrawer && <HeaderMenuButton navigation={navigation} /> }
-            { previous && !onBack && <HeaderBackButton navigation={navigation} /> }
-            { onBack && <HeaderBackButton onPress={onBack} /> }
-          </Box>
-          <Box flex>
-            <Heading size="small" textAlign='center'>
-              {title}
-            </Heading>
-          </Box>
-          <Box flex />
+    <Box background="foreground">
+      <Box style={{ paddingTop: insets.top }}></Box>
+      <Box pad="large" direction="row" align="center">
+        <Box flex>
+          {navigation && navigation.openDrawer && (
+            <HeaderMenuButton navigation={navigation} />
+          )}
+          {previous && !onBack && <HeaderBackButton navigation={navigation} />}
+          {onBack && <HeaderBackButton onPress={onBack} />}
         </Box>
-      </SafeAreaView>
+        <Box flex>
+          <Heading size="small" textAlign="center">
+            {title}
+          </Heading>
+        </Box>
+        <Box flex />
+      </Box>
     </Box>
   );
 }
