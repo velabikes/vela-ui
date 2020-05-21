60fd9385b3da9f317f724ac66408d5f66c095974
Diogo Vianna <diogo@velabikes.com.br>
2020-05-21 11:26:13 -0300

    Heading and subheading now support dynamic color based on background.

--- src/components/Typo/Heading.js
+++ src/components/Typo/Heading.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Text from '../Text'
-import {useTheme} from '../../lib/theme'
+import React from "react";
+import Text from "../Text";
+import { useTheme } from "../../lib/theme";
 
-function Heading({children, size, ...props}) {
-  const {typo: {heading}} = useTheme()
+function Heading({ children, size, ...props }) {
+  const {
+    typo: { heading },
+  } = useTheme();
 
   return (
-    <Text size={heading.size[size] || size || heading.size.medium} weight={'700'} {...props}>{children}</Text>
-  )
+    <Text
+      size={heading.size[size] || size || heading.size.medium}
+      weight={"700"}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
 }
 
-export default Heading
+export default Heading;
--- src/components/Typo/Subheading.js
+++ src/components/Typo/Subheading.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Text from '../Text'
 import {useTheme} from '../../lib/theme'
 
-function Heading({children, style, size}) {
+function Heading({children, style, size, ...props}) {
   const {typo: {subheading}} = useTheme()
 
   return (
-    <Text style={style} size={subheading.size[size] || size || subheading.size.medium} weight='600'>{children}</Text>
+    <Text
+      style={style}
+      size={subheading.size[size] || size || subheading.size.medium}
+      weight='600'
+      {...props}
+    >
+      {children}
+    </Text>
   )
 }
 
