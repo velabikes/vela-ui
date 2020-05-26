882cc9e5c3395b8f1f52a6dc3c6156c3b898b92c
Guilherme Oliveira <guilherme-lamon@hotmail.com>
2020-05-26 16:02:14 -0300

    fixed translate masked phoneNumber

--- src/components/Form/PhoneInput.js
+++ src/components/Form/PhoneInput.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import TextInput from './TextInput'
+import { useTheme } from '../../lib/theme'
+
 
 const phoneFormat = phone => phone.replace(/([^\d])+/gim, '')
-const maskPhone = value => value.replace(/^(\d{2})(\d{4,5})(\d{1,4}).*/,"$1 $2-$3")
+const maskByLocale = {
+  'pt-BR': value => value.replace(/^(\d{2})(\d{4,5})(\d{1,4}).*/,"$1 $2-$3"),
+  'en-US': value => value.replace(/^(\d{3})(\d{1,3})(\d{1,4}).*/,"$1 $2-$3")
+}
+const maskPhone = (value, locale) => maskByLocale[locale](value)
 
 const PhoneInput = ({onChangeText, value, ...props }) => {
+  const { locale } = useTheme()
+  const maskedPhone = maskPhone(value, locale)
   return (
     <TextInput
       onChangeText={text => {
         onChangeText(phoneFormat(text))
       }}
-      placeholder="00 00000-0000"
-      value={maskPhone(value)}
+      value={maskedPhone}
       keyboardType='numeric'
       {...props}
     />
