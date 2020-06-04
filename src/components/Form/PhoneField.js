ef9f12eb87611a8be48627b8d8e30e4844d66d94
Dagel <giovanna@velabikes.com.br>
2020-06-04 16:04:29 -0300

    PhoneField.js update

--- src/components/Form/PhoneField.js
+++ src/components/Form/PhoneField.js
@@ -2,9 +2,5 @@ import React from 'react'
 import PhoneInput from './PhoneInput'
 import FieldBase from './FieldBase'
 
-const validatePhone = value => {
-  if (value.length < 11) return 'Telefone incompleto.'
-}
-
 export default props =>
-  <FieldBase {...props} input={PhoneInput} validate={validatePhone} />
+  <FieldBase {...props} input={PhoneInput} />
