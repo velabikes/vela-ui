203aee92e1eda80937470261609e96b9c692c400
gidagel <giovanna@velabikes.com.br>
2020-06-25 19:27:51 -0300

    New version

--- src/components/Navbar/NavbarMenuButton.js
+++ src/components/Navbar/NavbarMenuButton.js
@@ -2,9 +2,12 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import Icons from "../Icons";
 
-export default function NavbarMenuButton({navigation}) {
+export default function NavbarMenuButton({ navigation }) {
   return (
-    <TouchableOpacity onPress={navigation && navigation.openDrawer}>
+    <TouchableOpacity
+      onPress={navigation && navigation.openDrawer}
+      hitSlop={{ top: 30, bottom: 30, left: 50, right: 50 }}
+    >
       <Icons.Menu />
     </TouchableOpacity>
   );
