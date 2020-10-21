2584d7d700d2a8f9cc145c7ad4201498adcc596f
Guilherme Muniz Cardoso <guilhermemcardoso@users.noreply.github.com>
2020-10-21 15:49:39 -0300

    Update PhoneInput.js

--- src/components/Form/PhoneInput.js
+++ src/components/Form/PhoneInput.js
@@ -5,8 +5,8 @@ import { useCountry } from '../../lib/useCountry'
 
 const phoneFormat = phone => phone.replace(/([^\d])+/gim, '')
 const maskByCountry = {
-  'BR': value => value.replace(/^(\d{2})(\d{4,5})(\d{1,4}).*/,"$1 $2-$3"),
-  'US': value => value.replace(/^(\d{3})(\d{1,3})(\d{1,4}).*/,"$1 $2-$3")
+  'BR': value => value?.replace(/^(\d{2})(\d{4,5})(\d{1,4}).*/,"$1 $2-$3"),
+  'US': value => value?.replace(/^(\d{3})(\d{1,3})(\d{1,4}).*/,"$1 $2-$3")
 }
 const maskPhone = (value, country) => maskByCountry[country](value)
 
