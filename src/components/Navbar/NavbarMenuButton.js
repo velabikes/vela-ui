175f0d6ced395ee2eba118d9c8cff6ad30562434
vinisaveg <viniisaveeg@gmail.com>
2021-08-19 15:21:28 -0300

    fix: add missing theme color to NavbarMenuButton Icon

--- src/components/Navbar/NavbarMenuButton.js
+++ src/components/Navbar/NavbarMenuButton.js
@@ -3,12 +3,14 @@ import { TouchableOpacity } from "react-native";
 import Icons from "../Icons";
 
 export default function NavbarMenuButton({ navigation }) {
+  const {colors} = useTheme()
+
   return (
     <TouchableOpacity
       onPress={navigation && navigation.openDrawer}
       hitSlop={{ top: 30, bottom: 30, left: 50, right: 50 }}
     >
-      <Icons.Menu />
+      <Icons.Menu style={{color: colors.title}}/>
     </TouchableOpacity>
   );
 }
